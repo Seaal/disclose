a27bbd609c179d4b324dcac5266afde877e9938d
Seaal <Seaal@users.noreply.github.com>
2016-06-09 16:38:39 +0100

    Resolve #6

---
+++ src/Disclose/ICommandHandlerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disclose.NET
+{
+    public static class ICommandHandlerExtensions
+    {
+        public static ICommandHandler WithCommandName(this ICommandHandler commandHandler, string newCommand)
+        {
+            return new NewCommandNameDecorator(commandHandler, newCommand);
+        }
+    }
+}
---
+++ src/Disclose/NewCommandNameDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Disclose.NET
+{
+    public class NewCommandNameDecorator : ICommandHandler
+    {
+        private readonly ICommandHandler _commandHandler;
+
+        public NewCommandNameDecorator(ICommandHandler commandHandler, string newCommandName)
+        {
+            _commandHandler = commandHandler;
+            Command = newCommandName;
+        }
+
+        public string Command { get; }
+
+        public string Description => _commandHandler.Description;
+        public Task Handle(DiscloseClient client, MessageEventArgs e, string arguments)
+        {
+            return _commandHandler.Handle(client, e, arguments);
+        }
+    }
+}
