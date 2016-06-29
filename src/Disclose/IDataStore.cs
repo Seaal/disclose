cf09805f8baf075a1b54cab37cab0920a83f4fda
Seaal <Seaal@users.noreply.github.com>
2016-06-29 22:22:33 +0100

    Resolve #17

--- src/Disclose/IDataStore.cs
+++ src/Disclose/IDataStore.cs
@@ -8,13 +8,13 @@ namespace Disclose
 {
     public interface IDataStore
     {
-        TData GetServerData<TData>(IServer server, string key);
-        void SetServerData<TData>(IServer server, string key, TData data);
+        Task<TData> GetServerData<TData>(IServer server, string key);
+        Task SetServerData<TData>(IServer server, string key, TData data);
 
-        TData GetUserData<TData>(IUser user, string key);
-        void SetUserData<TData>(IUser user, string key, TData data);
+        Task<TData> GetUserData<TData>(IUser user, string key);
+        Task SetUserData<TData>(IUser user, string key, TData data);
 
-        TData GetUserDataForServer<TData>(IServer server, IUser user, string key, TData data);
-        void SetUserData<TData>(IServer server, IUser user, string key, TData data);
+        Task<TData> GetUserDataForServer<TData>(IServer server, IUser user, string key, TData data);
+        Task SetUserData<TData>(IServer server, IUser user, string key, TData data);
     }
 }
