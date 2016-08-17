4be31002a8e8aa56657a4657570970af8c956b4c
Seaal <Seaal@users.noreply.github.com>
2016-08-17 17:09:32 +0100

    Added doc comments for IDataStore and fixed signatures of set/get UserDataForServer

--- src/Disclose/IDataStore.cs
+++ src/Disclose/IDataStore.cs
@@ -1,20 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Disclose.DiscordClient;
 using System.Threading.Tasks;
-using Disclose.DiscordClient;
 
 namespace Disclose
 {
+    /// <summary>
+    /// Persists data for Disclose handlers.
+    /// </summary>
     public interface IDataStore
     {
+        /// <summary>
+        /// Returns data for a server stored against the given key.
+        /// </summary>
+        /// <typeparam name="TData">The data type that is returned.</typeparam>
+        /// <param name="server">The server to return data for.</param>
+        /// <param name="key">The key to return data for.</param>
+        /// <returns>A Task representing the data stored against the given server/key.</returns>
         Task<TData> GetServerDataAsync<TData>(IServer server, string key);
+
+        /// <summary>
+        /// Adds/Updates data for a server using the given key.
+        /// </summary>
+        /// <typeparam name="TData">The data type of the data being stored.</typeparam>
+        /// <param name="server">The server to set the data for.</param>
+        /// <param name="key">The key to set the data for.</param>
+        /// <param name="data">The data to store for the given server/key.</param>
+        /// <returns>A Task representing the completion of the operation.</returns>
         Task SetServerDataAsync<TData>(IServer server, string key, TData data);
 
+        /// <summary>
+        /// Returns data for a user stored against the given key.
+        /// </summary>
+        /// <typeparam name="TData">The type of the data that is returned.</typeparam>
+        /// <param name="user">The data to return data for.</param>
+        /// <param name="key">The key to return data for.</param>
+        /// <returns>A Task representing the data stored against the given user/key.</returns>
         Task<TData> GetUserDataAsync<TData>(IUser user, string key);
+
+        /// <summary>
+        /// Adds/Updates data for a user using the given key.
+        /// </summary>
+        /// <typeparam name="TData">The data type of the data being stored.</typeparam>
+        /// <param name="user">The user to set the data for.</param>
+        /// <param name="key">The key to set the data for.</param>
+        /// <param name="data">The data to store for the given server/key.</param>
+        /// <returns>A Task representing the completion of the operation.</returns>
         Task SetUserDataAsync<TData>(IUser user, string key, TData data);
 
-        Task<TData> GetUserDataForServerAsync<TData>(IServer server, IUser user, string key, TData data);
-        Task SetUserDataForServerAsync<TData>(IServer server, IUser user, string key);
+        /// <summary>
+        /// Returns data specific to a user on a server stored against the given key. 
+        /// </summary>
+        /// <typeparam name="TData">The type of the data that is returned.</typeparam>
+        /// <param name="server">The server to return the data for.</param>
+        /// <param name="user">The user on the server to return the data for.</param>
+        /// <param name="key">The key to return the data for.</param>
+        /// <returns>A Task representing the data stored against the given server/user/key.</returns>
+        Task<TData> GetUserDataForServerAsync<TData>(IServer server, IUser user, string key);
+
+        /// <summary>
+        /// Adds/Updates data specific to a user on a server stored against the given key.
+        /// </summary>
+        /// <typeparam name="TData">The type of the data that is being stored.</typeparam>
+        /// <param name="server">The server to set the data for.</param>
+        /// <param name="user">The user to set the data for.</param>
+        /// <param name="key">The key to set the data for.</param>
+        /// <param name="data">The data to store for the given server/user/key.</param>
+        /// <returns>A Task representing the completion of the operation.</returns>
+        Task SetUserDataForServerAsync<TData>(IServer server, IUser user, string key, TData data);
     }
 }
