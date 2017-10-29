062b42c4a6cc21e800ab778fdd5170916919f141
Seaal <Seaal@users.noreply.github.com>
2017-10-29 01:44:18 -0400

    Removed unnecessary method on IDataStore

--- src/Disclose/IDataStore.cs
+++ src/Disclose/IDataStore.cs
@@ -44,26 +44,5 @@ namespace Disclose
         /// <param name="data">The data to store for the given server/key.</param>
         /// <returns>A Task representing the completion of the operation.</returns>
         Task SetUserDataAsync<TData>(DiscloseUser user, string key, TData data);
-
-        /// <summary>
-        /// Returns data specific to a user on a server stored against the given key. 
-        /// </summary>
-        /// <typeparam name="TData">The type of the data that is returned.</typeparam>
-        /// <param name="server">The server to return the data for.</param>
-        /// <param name="user">The user on the server to return the data for.</param>
-        /// <param name="key">The key to return the data for.</param>
-        /// <returns>A Task representing the data stored against the given server/user/key.</returns>
-        Task<TData> GetUserDataForServerAsync<TData>(DiscloseServer server, DiscloseUser user, string key);
-
-        /// <summary>
-        /// Adds/Updates data specific to a user on a server stored against the given key.
-        /// </summary>
-        /// <typeparam name="TData">The type of the data that is being stored.</typeparam>
-        /// <param name="server">The server to set the data for.</param>
-        /// <param name="user">The user to set the data for.</param>
-        /// <param name="key">The key to set the data for.</param>
-        /// <param name="data">The data to store for the given server/user/key.</param>
-        /// <returns>A Task representing the completion of the operation.</returns>
-        Task SetUserDataForServerAsync<TData>(DiscloseServer server, DiscloseUser user, string key, TData data);
     }
 }
